502d87d0880db084dcd7906f8ef89d965876f062
georgeman93 <george.gpo93@gmail.com>
2020-03-26 19:42:07 +1100

    Updates

---
+++ precache-manifest.dffb788c2e256a92b1f55d7221afdf78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24108455f5df1f162e0c53569cfa9550",
+    "url": "/JEreact/index.html"
+  },
+  {
+    "revision": "778fd00b9c39023304c9",
+    "url": "/JEreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "828933907deae6c2b8d5",
+    "url": "/JEreact/static/js/2.6b8939ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JEreact/static/js/2.6b8939ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778fd00b9c39023304c9",
+    "url": "/JEreact/static/js/main.c9eeb3ef.chunk.js"
+  },
+  {
+    "revision": "9766c37a03498d06d4c1",
+    "url": "/JEreact/static/js/runtime-main.bcb2ec97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JEreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
